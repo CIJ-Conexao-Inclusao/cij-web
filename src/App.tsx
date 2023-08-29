ea3f4a7f7f25caaccb234839277fa7fdd4651943
Kenzo Sato <kenzo_sato@estudante.sc.senai.br>
2023-08-28 21:37:28 -0300

    feat: add color config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+import { Button } from "@mui/material";
+import TemaProvider, { ColorModeContext } from "./providers/TemaProvider";
+import { useContext } from "react";
 
-function App() {
-  return <h1>Hello world</h1>;
-}
+const App = () => {
+  const { toggleColorMode, mode } = useContext(ColorModeContext);
+
+  const handleToggleMode = () => {
+    console.log("clicou!");
+    toggleColorMode();
+  };
+
+  return (
+    <TemaProvider>
+      <Button variant="contained" onClick={handleToggleMode}>
+        Text
+      </Button>
+      {mode}
+      <h1>Hello world</h1>
+    </TemaProvider>
+  );
+};
 
 export default App;
---
+++ src/providers/TemaProvider.tsx
@@ -0,0 +1,59 @@
+import { createContext, useMemo, useState } from "react";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline, PaletteMode } from "@mui/material";
+import { getDesignTokens } from "../configs/TemaConfig";
+
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {},
+  mode: "light",
+});
+
+const TemaProvider = (props: any) => {
+  const [mode, setMode] = useState("light");
+
+  //   const colorMode = useMemo(
+  //     () => ({
+  //       toggleColorMode: (mode: PaletteMode) => {
+  //         setMode(mode);
+  //       },
+  //       mode,
+  //     }),
+  //     [mode]
+  //   );
+
+  //   const colorMode = useMemo(
+  //     () => ({
+  //       toggleColorMode: () => {
+  //         console.log("ta passando aqui?");
+  //         setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+  //       },
+  //       mode,
+  //     }),
+  //     []
+  //   );
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        console.log("passou ");
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+      mode,
+    }),
+    [mode]
+  );
+
+  const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {props.children}
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
+
+export default TemaProvider;
