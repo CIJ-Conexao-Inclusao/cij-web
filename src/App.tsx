c125a5f6cd8c5ca5b1b32693dae63e7d05f6062f
KENZO HIDEAKY FERREIRA SATO <kenzo_sato@estudante.sc.senai.br>
2023-09-04 21:18:54 -0300

    feat: first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import "./App.css";
-
-import { Button } from "@mui/material";
-import TemaProvider, { ColorModeContext } from "./providers/TemaProvider";
 import { useContext } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+
+import TemaProvider, { ColorModeContext } from "./providers/TemaProvider";
+
+import CookieService from "./services/CookieService";
 
 const App = () => {
   const { toggleColorMode, mode } = useContext(ColorModeContext);
@@ -14,13 +25,42 @@ const App = () => {
 
   return (
     <TemaProvider>
-      <Button variant="contained" onClick={handleToggleMode}>
-        Text
-      </Button>
-      {mode}
-      <h1>Hello world</h1>
+      <div className="text-red-500">tesets</div>
+      <p className="text-3xl font-bold underline">Hello world!</p>
+      <Router>
+        <Routes>
+          <Route path="/signin" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </TemaProvider>
   );
+};
+
+type ProtectedRouteProps = {
+  tiposUsuarioAllowed: string;
+  children: any;
+  redirectPath: string;
+};
+
+const ProtectedRoute = ({
+  tiposUsuarioAllowed = "",
+  children,
+  redirectPath = "/signin",
+}: ProtectedRouteProps) => {
+  const cookie = CookieService.getCookie("jwt");
+  const userJpa = CookieService.getCookie("user");
+
+  if (
+    cookie != null &&
+    cookie.exp > Math.floor(Date.now() / 1000) &&
+    (tiposUsuarioAllowed.includes(userJpa.authorities[0].authority) ||
+      tiposUsuarioAllowed == "")
+  ) {
+    return children ? children : <Outlet />;
+  } else {
+    return <Navigate to={redirectPath} replace />;
+  }
 };
 
 export default App;
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,49 @@
-// import React from "react";
+import * as React from "react";
+import { useState } from "react";
+
+import { Box, Button, TextField } from "@mui/material";
+
+import { ILogin } from "../../interfaces";
+
+import LoginService from "../../services/LoginService";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [user, setUser] = useState<ILogin>({
+    email: "",
+    password: "",
+  });
+
+  const login = async () => {
+    const res = await LoginService.login(user);
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser({ ...user, email: e.target.value });
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser({ ...user, password: e.target.value });
+  };
+
+  return (
+    <Box className="flex flex-col">
+      <div className="flex flex-col">
+        <TextField
+          variant="standard"
+          placeholder="Email"
+          onChange={handleEmailChange}
+        />
+        <TextField
+          variant="standard"
+          placeholder="Senha"
+          onChange={handlePasswordChange}
+        />
+        <Button variant="contained" disableElevation onClick={login}>
+          Login
+        </Button>
+      </div>
+    </Box>
+  );
 };
 
 export default Login;
