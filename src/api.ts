06afc514510b02ff5e540b9a0de33b3da2588db6
Kenzo Sato <kenzo_sato@estudante.sc.senai.br>
2023-09-25 20:48:10 -0300

    feat: add component

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:3040/cij/api/v1/",
+    baseURL: "http://15.228.164.199:3040/cij/api/v1/",
     withCredentials: true
 })
 
