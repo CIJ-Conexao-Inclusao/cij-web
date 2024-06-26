a84c1cbfde40897a850c8985ad25ee0dcb82ed50
pessotticamilly <camilly_pessotti@estudante.sc.senai.br>
2024-06-26 20:55:14 -0300

    refactor: updating inputs

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
 
-import { Box, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, Step, StepLabel, Stepper} from "@mui/material";
+import { Box, FormControl, FormLabel } from "@mui/material";
 import { BoxRightColumn, BoxLeftColumn, BoxLogoImage, BoxBackgroundImage, BoxTitle, BoxInputs, BoxButtons } from "./SignUp.styled";
 import { Inputs, PrimaryButton } from "../../App.styled";
 import PersonalData from "./Steps/PersonalData/PersonalData.tsx";
@@ -21,50 +20,52 @@ import { UserService } from "../../services/index.ts";
 import { Link, useNavigate } from "react-router-dom";
 import { useToast } from "../../hooks/useToast.tsx";
 
+import React, { useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { Stepper, Step, StepLabel, Button, TextField, RadioGroup, FormControlLabel, Radio, MenuItem } from '@mui/material';
+import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
+import AdapterDateFns from '@mui/x-date-pickers/AdapterDateFns';
+import axios from 'axios';
+
+import InputMask from 'react-input-mask';
+
+
+const steps = ['Dados Pessoais', 'Deficiência', 'Endereço'];
+
 const SignUp = () => {
-	const toast = useToast();
-	const navigate = useNavigate();
+	const [activeStep, setActiveStep] = useState(0);
+    const { handleSubmit, control, watch, setValue } = useForm();
+    const [cepData, setCepData] = useState(null);
 
-	// Paginação
-	const steps = [
-		'Dados Pessoais',
-		'Deficiência',
-		'Endereço',
-	];
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
 
-	const [step, setStep] = useState(0);
-	const [userData, setUserData] = useState({
-		personalData: {},
-		disability: {},
-		address: {},
-	});
-	  
-	const handleNext = (data: { personalData: {}; disability: {}; address: {}; }) => {
-		setUserData(prevData => ({
-			...prevData,
-			...data
-		}))
-		setStep((prevStep) => prevStep + 1);
-	};
-	  
-	const handleBack = () => {
-		setStep((prevStep) => prevStep - 1);
-	};
-	  
-	const renderStepContent = (step: number) => {
-		switch (step) {
-		case 0:
-			return <PersonalData onNext={handleNext} />;
-		case 1:
-			return <Disability onNext={handleNext} />;
-		case 2:
-			return <Address onNext={handleNext} />;
-		default:
-			return null;
-		}
-	};
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
 
-	const isLastStep = step === steps.length - 1;
+    const onSubmit = (data: any) => {
+        console.log(data);
+    };
+
+    const handleCepBlur = async () => {
+        const cep = watch('cep');
+        if (cep && cep.replace(/\D/g, '').length === 8) {
+            try {
+                const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+                if (response.data) {
+                    setValue('pais', 'Brasil');
+                    setValue('estado', response.data.uf);
+                    setValue('cidade', response.data.localidade);
+                    setValue('bairro', response.data.bairro);
+                    setValue('rua', response.data.logradouro);
+                }
+            } catch (error) {
+                console.error('Erro ao buscar o CEP:', error);
+            }
+        }
+    };
 
 	// const signUp = () => {
 	// 	userSchema
@@ -104,38 +105,194 @@ const SignUp = () => {
 				</BoxBackgroundImage>
 			</BoxLeftColumn>
 			<BoxRightColumn>
-      			<Stepper activeStep={step} alternativeLabel>
-        			{steps.map((label) => (
-          				<Step key={label}>
-            				<StepLabel>{label}</StepLabel>
-          				</Step>
-        			))}
-      			</Stepper>
-				  <Box>
-        			{renderStepContent(step)}
-        			<Box>
-          				{step !== 0 && (
-            				<button onClick={handleBack}>Voltar</button>
-          				)}
-          				{step < steps.length - 1 && (
-            				<button onClick={() => setStep(prevStep => prevStep + 1)}>Próximo</button>
-          				)}
-          				{isLastStep && (
-            				<button onClick={signUp}>Cadastrar</button>
-          				)}
-        			</Box>
-      			</Box>
-				<BoxButtons>
-					{/* <PrimaryButton onClick={signUp}>
-						Cadastrar
-					</PrimaryButton> */}
-					<p className="little-text">
-						Já possui uma conta?{" "}
-						<Link to={ROUTES.signIn} className="link">
-							Login
-						</Link>
-					</p>
-				</BoxButtons>
+			<form onSubmit={handleSubmit(onSubmit)}>
+            <Stepper activeStep={activeStep}>
+                {steps.map((label) => (
+                    <Step key={label}>
+                        <StepLabel>{label}</StepLabel>
+                    </Step>
+                ))}
+            </Stepper>
+            {activeStep === 0 && (
+                <>
+                    <Controller
+                        name="nome"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => <TextField {...field} label="Nome Completo" fullWidth margin="normal" />}
+                    />
+                    <Controller
+                        name="cpf"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => (
+                            <InputMask mask="999.999.999-99" {...field} maskChar={null}>
+                                {(inputProps: any) => <TextField {...inputProps} label="CPF" fullWidth margin="normal" />}
+                            </InputMask>
+                        )}
+                    />
+                    <Controller
+                        name="dataNascimento"
+                        control={control}
+                        defaultValue={null}
+                        render={({ field }) => (
+                            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                <DatePicker
+                                    {...field}
+                                    label="Data de Nascimento"
+                                    renderInput={(props: any) => <TextField {...props} fullWidth margin="normal" />}
+                                />
+                            </LocalizationProvider>
+                        )}
+                    />
+                    <Controller
+                        name="genero"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => (
+                            <RadioGroup {...field} row>
+                                <FormControlLabel value="masculino" control={<Radio />} label="Masculino" />
+                                <FormControlLabel value="feminino" control={<Radio />} label="Feminino" />
+                                <FormControlLabel value="outro" control={<Radio />} label="Outro" />
+                            </RadioGroup>
+                        )}
+                    />
+                    <Controller
+                        name="celular"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => (
+                            <InputMask mask="(99) 99999-9999" {...field}>
+                                {(inputProps: any) => <TextField {...inputProps} label="Celular" fullWidth margin="normal" />}
+                            </InputMask>
+                        )}
+                    />
+                    <Controller
+                        name="email"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => <TextField {...field} label="E-mail" fullWidth margin="normal" />}
+                    />
+                    <Controller
+                        name="senha"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => <TextField {...field} type="password" label="Senha" fullWidth margin="normal" />}
+                    />
+                    <Controller
+                        name="confirmarSenha"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => <TextField {...field} type="password" label="Confirmar Senha" fullWidth margin="normal" />}
+                    />
+                </>
+            )}
+            {activeStep === 1 && (
+                <>
+                    <Controller
+                        name="tipoDeficiencia"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => (
+                            <TextField {...field} select label="Tipo de Deficiência" fullWidth margin="normal">
+                                <MenuItem value="auditiva">Auditiva</MenuItem>
+                                <MenuItem value="fisica">Física</MenuItem>
+                                <MenuItem value="intelectual">Intelectual</MenuItem>
+                                <MenuItem value="psicossocial">Psicossocial</MenuItem>
+                                <MenuItem value="visual">Visual</MenuItem>
+                            </TextField>
+                        )}
+                    />
+                    <Controller
+                        name="deficiencia"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => <TextField {...field} label="Deficiência" fullWidth margin="normal" />}
+                    />
+                    <Controller
+                        name="grauSubdivisao"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => <TextField {...field} label="Grau/Subdivisão" fullWidth margin="normal" />}
+                    />
+                    <Controller
+                        name="deficienciaAdquirida"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => (
+                            <RadioGroup {...field} row>
+                                <FormControlLabel value="sim" control={<Radio />} label="Sim" />
+                                <FormControlLabel value="nao" control={<Radio />} label="Não" />
+                            </RadioGroup>
+                        )}
+                    />
+                </>
+            )}
+            {activeStep === 2 && (
+                <>
+                    <Controller
+                        name="cep"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => (
+                            <InputMask mask="99999-999" {...field}>
+                                {(inputProps: any) => <TextField {...inputProps} label="CEP" fullWidth margin="normal" onBlur={handleCepBlur} />}
+                            </InputMask>
+                        )}
+                    />
+                    <Controller
+                        name="pais"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => <TextField {...field} label="País" fullWidth margin="normal" />}
+                    />
+                    <Controller
+                        name="estado"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => <TextField {...field} label="Estado" fullWidth margin="normal" />}
+                    />
+                    <Controller
+                        name="cidade"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => <TextField {...field} label="Cidade" fullWidth margin="normal" />}
+                    />
+                    <Controller
+                        name="bairro"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => <TextField {...field} label="Bairro" fullWidth margin="normal" />}
+                    />
+                    <Controller
+                        name="rua"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => <TextField {...field} label="Rua" fullWidth margin="normal" />}
+                    />
+                    <Controller
+                        name="numero"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => <TextField {...field} label="Número" fullWidth margin="normal" />}
+                    />
+                    <Controller
+                        name="complemento"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => <TextField {...field} label="Complemento" fullWidth margin="normal" />}
+                    />
+                </>
+            )}
+            <div>
+                <Button disabled={activeStep === 0} onClick={handleBack}>
+                    Voltar
+                </Button>
+                <Button variant="contained" color="primary" onClick={handleNext}>
+                    {activeStep === steps.length - 1 ? 'Finalizar' : 'Próximo'}
+                </Button>
+            </div>
+        </form>
 			</BoxRightColumn>
 		</Box>
 	);
