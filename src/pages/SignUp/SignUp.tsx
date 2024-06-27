409312fbbe03bc49600935cb7fd6369b59b19e2f
camillys <camillys@weg.net>
2024-06-27 16:46:00 -0300

    refactor: updating sign up

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,9 +1,8 @@
-import { Box, FormControl, FormLabel } from "@mui/material";
-import { BoxRightColumn, BoxLeftColumn, BoxLogoImage, BoxBackgroundImage, BoxTitle, BoxInputs, BoxButtons } from "./SignUp.styled";
+import React, { useEffect, useMemo, useState } from "react";
+
+import { Box, TextField, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, Step, StepLabel, Stepper, Typography } from "@mui/material";
+import { BoxRightColumn, BoxLeftColumn, BoxLogoImage, BoxBackgroundImage } from "./SignUp.styled";
 import { Inputs, PrimaryButton } from "../../App.styled";
-import PersonalData from "./Steps/PersonalData/PersonalData.tsx";
-import Disability from "./Steps/Disability/Disability.tsx";
-import Address from "./Steps/Address/Address.tsx";
 
 import "./SignUp.scss";
 
@@ -12,29 +11,15 @@ import signUpBackground from "./assets/sign-up-background.png";
 
 import { userSchema } from "../../validations/index.ts";
 
-import { GENDER, ROUTES } from "../../constants/index.ts";
-import { TUserForm } from "../../types/index.ts";
+import { GENDER, ADQUIREDDISABILITY, ROUTES } from "../../constants/index.ts";
+import { TUserForm, TUserDisability, TUserAddress } from "../../types/TUserForm.ts";
 
+import { UserService } from "../../services/index.ts";
 import { Link, useNavigate } from "react-router-dom";
 import { useToast } from "../../hooks/useToast.tsx";
 import { useTranslation } from "react-i18next";
 
-import React, { useEffect, useMemo, useState } from 'react';
-import { useForm, Controller } from 'react-hook-form';
-import { Stepper, Step, StepLabel, Button, TextField, RadioGroup, FormControlLabel, Radio, MenuItem } from '@mui/material';
-import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
-import AdapterDateFns from '@mui/x-date-pickers/AdapterDateFns';
-import axios from 'axios';
-
-import InputMask from 'react-input-mask';
-
-import { ColDef } from "ag-grid-enterprise";
-
-import { t } from "i18next";
-import { useFontSize } from "../../hooks/useFontSize.tsx";
-import { useSwitchTheme } from "../../hooks/useSwitchTheme.tsx";
-import UserService from "../../services/UserService.ts";
-import TUser from "../../types/TUser.ts";
+import { useFontSize } from "../../hooks/useFontSize";
 
 
 const steps = ['Dados Pessoais', 'Deficiência', 'Endereço'];
@@ -42,155 +27,82 @@ const steps = ['Dados Pessoais', 'Deficiência', 'Endereço'];
 const SignUp = () => {
 	const toast = useToast();
 	const { fontSizeConfig } = useFontSize();
-	const { themeMode } = useSwitchTheme();
+	const navigate = useNavigate();
+	const { t } = useTranslation("translation", { keyPrefix: "signUp" });
+	const { t: tErrors } = useTranslation("translation", {
+		keyPrefix: "errors",
+	});
 
-	const [companies, setCompanies] = useState<TUser[]>([]);
-	const [showFormModal, setShowFormModal] = useState<boolean>(false);
+	const [user, setUser] = useState<TUserForm>({
+		name: "",
+		cpf: "",
+		birthDate: "",
+		gender: GENDER.Male,
+		phone: "",
+		email: "",
+		password: ""
+	});
+
+	const [userDisability, setUserDisability] = useState<TUserDisability>({
+		disabilityType: "",
+		disability: "",
+		disablityDegree: "",
+		adquiredDisability: ADQUIREDDISABILITY.No
+	});
+
+	const [userAddress, setUserAddress] = useState<TUserAddress>({
+		zip_code: "",
+		country: "",
+		state: "",
+		city: "",
+		neighborhood: "",
+		street: "",
+		number: "",
+		complement: "",
+	});
+
+	const [confirmPassword, setConfirmPassword] = useState<string>("");
+
+	const handleUserChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setUser({ ...user, [e.target.name]: e.target.value });
+	};
+
+	const handleUserDisabilityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setUserDisability({ ...userDisability, [e.target.name]: e.target.value });
+	};
 	
+	const handleUserAddressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setUserAddress({ ...userAddress, [e.target.name]: e.target.value });
+	};
+
 	const [activeStep, setActiveStep] = useState(0);
 
-	const [rowData, setRowData] = useState<TUser[]>([]);
 
-	const agGridTheme = useMemo(() => {
-		return themeMode === "light"
-			? "ag-theme-quartz"
-			: "ag-theme-quartz-dark";
-	}, [themeMode]);
+	const signUp = () => {
+		userSchema
+			.validate(user)
+			.then(() => {
+				if (user.password !== confirmPassword) {
+					toast.showToast("error", "As senhas não coincidem");
+					return;
+				}
 
-	const steps = ["Dados Pessoais", "Deficiência", "Ender"];
-
-	const handleUserChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setUserForm({ ...UserForm, [e.target.name]: e.target.value });
-	};
-
-	const handleUserChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setUserForm({ ...userForm, [e.target.name]: e.target.value });
-	};
-
-	const handleAddressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setAdressForm({ ...addressForm, [e.target.name]: e.target.value });
-	};
-
-	const [gridApi, setGridApi] = useState<any>(null);
-
-	const handleOnGridReady = (params: any) => {
-		setGridApi(params.api);
-		params.api.sizeColumnsToFit();
-	};
-
-	useEffect(() => {
-		if (gridApi && companies) setRowData(companies);
-	}, [companies, gridApi]);
-
-	useEffect(() => {
-		if (gridApi && rowData) gridApi.sizeColumnsToFit();
-	}, [rowData]);
-
-	const style = {
-		position: "absolute" as "absolute",
-		top: "50%",
-		left: "50%",
-		transform: "translate(-50%, -50%)",
-		width: 500,
-		bgcolor: "background.paper",
-		boxShadow: 24,
-		p: 2,
-		borderRadius: 2,
-	};
-
-	const allFieldsFilled = useMemo(() => {
-		switch (activeStep) {
-			case 0:
-				return (
-					!UserForm.cnpj || !UserForm.name || !UserForm.phone
-				);
-			case 1:
-				return (
-					!addressForm.city ||
-					!addressForm.country ||
-					!addressForm.neighborhood ||
-					!addressForm.number ||
-					!addressForm.state ||
-					!addressForm.street ||
-					!addressForm.zip_code
-				);
-			case 2:
-				return !userForm.email || !userForm.password;
-			default:
-				return true;
-		}
-	}, [addressForm, userForm, UserForm, activeStep]);
-
-	const handleCancelForm = () => {
-		setShowFormModal(false);
-		reset();
-	};
-
-	const reset = () => {
-		setActiveStep(0);
-		setUserForm({
-			cnpj: "",
-			name: "",
-			phone: "",
-		});
-		setAdressForm({
-			city: "",
-			complement: "",
-			country: "",
-			neighborhood: "",
-			number: "",
-			state: "",
-			street: "",
-			zip_code: "",
-		});
-		setUserForm({
-			email: "",
-			password: "",
-		});
-	};
-
-	const createUser = async () => {
-		try {
-			await UserService.create({
-				...UserForm,
-				address: addressForm,
-				user: userForm,
+				UserService.create(user)
+					.then(() => {
+						toast.showToast("success", "Usuário cadastrado com sucesso");
+						navigate(ROUTES.signIn);
+					})
+					.catch((err) => {
+						console.log("err: ", err);
+						toast.showToast(
+						"error", "Erro ao cadastrar usuário"
+						);
+					});
+			})
+			.catch((err) => {
+				toast.showToast("error", err.errors[0]);
 			});
-			toast.showToast("success", "Empresa criada com sucesso");
-			setShowFormModal(false);
-			reset();
-			getCompanies();
-		} catch (error: any) {
-			toast.showToast("error", "Falha ao criar empresa");
-		}
 	};
-
-	// const signUp = () => {
-	// 	userSchema
-	// 		.validate(user)
-	// 		.then(() => {
-	// 			if (user.password !== confirmPassword) {
-	// 				toast.showToast("error", "As senhas não coincidem");
-	// 				return;
-	// 			}
-
-	// 			UserService.create(user)
-	// 				.then(() => {
-	// 					toast.showToast(
-	// 						"success",
-	// 						"Usuário criado com sucesso"
-	// 					);
-	// 					navigate(ROUTES.signIn);
-	// 				})
-	// 				.catch((err) => {
-	// 					console.log("err: ", err);
-	// 					toast.showToast("error", "Erro ao criar usuário");
-	// 				});
-	// 		})
-	// 		.catch((err) => {
-	// 			toast.showToast("error", err.errors[0]);
-	// 		});
-	// };
 
 	return (
 		<Box sx={{ display: "flex" }}>
@@ -221,29 +133,68 @@ const SignUp = () => {
 				{activeStep === 0 ? (
 
 					<>
-						<InputStyled variant="outlined" placeholder={"CNPJ"} name="cnpj" value={companyForm.cnpj} onChange={handleCompanyChange} size="small" required />
-						<InputStyled variant="outlined" placeholder={"Nome"} name="name" value={companyForm.name} onChange={handleCompanyChange} size="small" required />
-						<InputStyled variant="outlined" placeholder={"Número"} name="phone" value={companyForm.phone} onChange={handleCompanyChange} size="small" required />
+						<Inputs variant="outlined" placeholder={"Nome Completo"} name="name" value={user.name} onChange={handleUserChange} size="small" required />
+						<Inputs variant="outlined" placeholder={"CPF"} name="cpf" value={user.cpf} onChange={handleUserChange} size="small" required />
+						<Inputs variant="outlined" placeholder={"Data de nascimento"} name="birthDate" value={user.birthDate} onChange={handleUserChange} size="small" required />
+						<FormControl
+							sx={{
+								color: "#999",
+								marginBottom: "2rem",
+								width: "20vw",
+							}}
+						>
+							<FormLabel sx={{ color: "#999" }}>
+								Gênero
+							</FormLabel>
+							<RadioGroup row name="gender" value={user.gender} onChange={handleUserChange}>
+								<FormControlLabel value={GENDER.Male} control={<Radio />} label="Masculino" />
+								<FormControlLabel value={GENDER.Female} control={<Radio />} label="Feminino" />
+								<FormControlLabel value={GENDER.Other} control={<Radio />} label="Outro" />
+							</RadioGroup>
+						</FormControl>
+						
+						<Inputs variant="outlined" placeholder={"Celular"} name="phone" value={user.phone} onChange={handleUserChange} size="small" required />
+						<Inputs variant="outlined" placeholder={"E-mail"} name="email" value={user.email} onChange={handleUserChange} size="small" required />
+						<Inputs variant="outlined" placeholder={"Senha"} name="password" value={user.password} onChange={handleUserChange} size="small" required />
+						<Inputs variant="outlined" placeholder={"Confirmar senha"} name="confirmPassword" value={user.name} onChange={handleUserChange} size="small" required />
 					</>
 
 				) : activeStep === 1 ? (
 
 					<>
-						<InputStyled variant="outlined" placeholder={"País"} name="country" value={addressForm.country} onChange={handleAddressChange} size="small" required />
-						<InputStyled variant="outlined" placeholder={"CEP"} name="zip_code" value={addressForm.zip_code} onChange={handleAddressChange} size="small" required />
-						<InputStyled variant="outlined" placeholder={"Estado"} name="state" value={addressForm.state} onChange={handleAddressChange} size="small" required />
-						<InputStyled variant="outlined" placeholder={"Cidade"} name="city" value={addressForm.city} onChange={handleAddressChange} size="small" required />
-						<InputStyled variant="outlined" placeholder={"Bairro"} name="neighborhood" value={addressForm.neighborhood} onChange={handleAddressChange} size="small" required />
-						<InputStyled variant="outlined" placeholder={"Rua"} name="street" value={addressForm.street} onChange={handleAddressChange} size="small" required />
-						<InputStyled variant="outlined" placeholder={"Número"} name="number" value={addressForm.number} onChange={handleAddressChange} size="small" required />
-						<InputStyled variant="outlined" placeholder={"Complement"} name="complement" value={addressForm.complement} onChange={handleAddressChange} size="small" required />
+						<Inputs variant="outlined" placeholder={"Tipo de deficiência"} name="disabilityType" value={userDisability.disabilityType} onChange={handleUserDisabilityChange} size="small" required />
+						<Inputs variant="outlined" placeholder={"Deficiência"} name="disability" value={userDisability.disability} onChange={handleUserDisabilityChange} size="small" required />
+						<Inputs variant="outlined" placeholder={"Grau/Subdivisão"} name="disablityDegree" value={userDisability.disablityDegree} onChange={handleUserDisabilityChange} size="small" required />
+						
+						<FormControl
+							sx={{
+								color: "#999",
+								marginBottom: "2rem",
+								width: "20vw",
+							}}
+						>
+							<FormLabel sx={{ color: "#999" }}>
+								A deficiência foi adquirida?
+							</FormLabel>
+
+							<RadioGroup row name="adquiredDisability" value={userDisability.adquiredDisability} onChange={handleUserDisabilityChange}>
+								<FormControlLabel value={ADQUIREDDISABILITY.Yes} control={<Radio />} label="Sim" />
+								<FormControlLabel value={ADQUIREDDISABILITY.No} control={<Radio />} label="Não" />
+							</RadioGroup>
+						</FormControl>
 					</>
 
 				) : activeStep === 2 ? (
 
 					<>
-						<InputStyled variant="outlined" placeholder={"Email"} name="email" value={userForm.email} onChange={handleUserChange} size="small" required />
-						<InputStyled variant="outlined" placeholder={"Senha"} name="password" type="password" value={userForm.password} onChange={handleUserChange} size="small" required />
+						<Inputs variant="outlined" placeholder={"CEP"} name="zip_code" value={userAddress.zip_code} onChange={handleUserAddressChange} size="small" required />
+						<Inputs variant="outlined" placeholder={"País"} name="country" value={userAddress.country} onChange={handleUserAddressChange} size="small" required />
+						<Inputs variant="outlined" placeholder={"Estado"} name="state" value={userAddress.state} onChange={handleUserAddressChange} size="small" required />
+						<Inputs variant="outlined" placeholder={"Cidade"} name="city" value={userAddress.city} onChange={handleUserAddressChange} size="small" required />
+						<Inputs variant="outlined" placeholder={"Bairro"} name="neighborhood" value={userAddress.neighborhood} onChange={handleUserAddressChange} size="small" required />
+						<Inputs variant="outlined" placeholder={"Rua"} name="street" value={userAddress.street} onChange={handleUserAddressChange} size="small" required />
+						<Inputs variant="outlined" placeholder={"Número"} name="number" value={userAddress.number} onChange={handleUserAddressChange} size="small" required />
+						<Inputs variant="outlined" placeholder={"Complemento"} name="complement" value={userAddress.complement} onChange={handleUserAddressChange} size="small" required />
 					</>
 
 				) : (
