deedad020fd48c259b96cabcd957f0538de4b0f7
Kenzo Sato <kenzo_sato@estudante.sc.senai.br>
2024-10-18 15:50:29 -0300

    feat: testing map lib

--- src/components/MapChart/MapChart.tsx
+++ src/components/MapChart/MapChart.tsx
@@ -1,6 +1,8 @@
 import * as ChartGeo from "chartjs-chart-geo";
 import React, { useEffect, useRef } from "react";
 import { feature } from "topojson-client";
+// import tst from "../../assets/geojson/jaragua-do-sul-bairros-tst.json";
+import GeoJson from "../../assets/geojson/jaragua-do-sul-bairros.json";
 
 const MapChart = () => {
   const chartRef = useRef<HTMLCanvasElement | null>(null);
@@ -9,6 +11,8 @@ const MapChart = () => {
   useEffect(() => {
     const url = "https://unpkg.com/world-atlas@2.0.2/countries-50m.json";
 
+    if (!chartRef.current) return;
+
     fetch(url)
       .then((result) => result.json())
       .then((datapoint) => {
@@ -16,22 +20,64 @@ const MapChart = () => {
           feature(datapoint, datapoint.objects.countries) as any
         ).features;
 
-        console.log(countries);
+        console.log("datapoint", datapoint);
+        console.log("countries", countries);
+
+        // console.log("geoJson", GeoJson);
+        // console.log("features", nbs);
+
+        // const data = {
+        //   labels: countries.map((country: any) => country.properties.name),
+        //   datasets: [
+        //     {
+        //       label: "Countries",
+        //       data: countries.map((country: any) => ({
+        //         feature: country,
+        //         value: Math.random(),
+        //       })),
+        //     },
+        //   ],
+        // };
+
+        // const config: any = {
+        //   type: "choropleth",
+        //   data,
+        //   options: {
+        //     showOutline: true,
+        //     showGraticule: true,
+        //     scales: {
+        //       xy: {
+        //         projection: "equalEarth",
+        //       },
+        //     },
+        //     plugins: {
+        //       legend: {
+        //         display: false,
+        //       },
+        //     },
+        //   },
+        // };
+
+        // new ChartGeo.ChoroplethChart(
+        //   chartRef.current as HTMLCanvasElement,
+        //   config
+        // );
+
+        // WORKING
+        const nbs = GeoJson.features;
 
         const data = {
-          labels: countries.map((country: any) => country.properties.name),
+          labels: nbs.map((nb) => nb.properties.name || "Sem nome"),
           datasets: [
             {
-              label: "Countries",
-              data: countries.map((country: any) => ({
-                feature: country,
+              label: "Bairros",
+              data: nbs.map((nb) => ({
+                feature: nb,
                 value: Math.random(),
               })),
             },
           ],
         };
-
-        console.log(data);
 
         const config: any = {
           type: "choropleth",
@@ -40,9 +86,7 @@ const MapChart = () => {
             showOutline: true,
             showGraticule: true,
             scales: {
-              xy: {
-                projection: "equalEarth",
-              },
+              xy: {},
             },
             plugins: {
               legend: {
@@ -56,6 +100,50 @@ const MapChart = () => {
           chartRef.current as HTMLCanvasElement,
           config
         );
+
+        // COULDN'T MAKE IT WORK
+        //   const nbs = (
+        //     ChartGeo.topojson.feature(
+        //       tst as any,
+        //       tst.objects.tst.features as any
+        //     ) as any
+        //   ).features;
+        //   console.log("aqui", nbs);
+
+        //   const data = {
+        //     labels: nbs.map((nb: any) => nb.properties.name || "Sem nome"),
+        //     datasets: [
+        //       {
+        //         label: "Bairros",
+        //         data: nbs.map((nb: any) => ({
+        //           feature: nb,
+        //           value: Math.random(),
+        //         })),
+        //       },
+        //     ],
+        //   };
+
+        //   const config: any = {
+        //     type: "choropleth",
+        //     data,
+        //     options: {
+        //       showOutline: true,
+        //       showGraticule: true,
+        //       scales: {
+        //         xy: {},
+        //       },
+        //       plugins: {
+        //         legend: {
+        //           display: false,
+        //         },
+        //       },
+        //     },
+        //   };
+
+        //   new ChartGeo.ChoroplethChart(
+        //     chartRef.current as HTMLCanvasElement,
+        //     config
+        //   );
       });
   }, []);
 
